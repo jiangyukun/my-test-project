ef5d23d616d8e8bd297778c0760a5a2561a5bd6d
jiangyukun <187_6810_5877@sina.com>
2019-12-01 22:36:59 +0800

    给接口返回值添加类型

--- src/webpack/tapable/AsyncSeriesHook.js
+++ src/webpack/tapable/AsyncSeriesHook.js
@@ -3,6 +3,7 @@ const {AsyncSeriesHook} = require("tapable")
 let hook = new AsyncSeriesHook(['factory'])
 
 hook.tapAsync('test', (d, callback) => {
+    console.log(d);
     setTimeout(() => {
         callback()
     }, 1000)
@@ -12,17 +13,14 @@ hook.tapAsync('test', (d, callback) => {
     callback(55)
 })
 
-hook.tapPromise('test', (d,) => {
-    return Promise.resolve(333)
-})
-
-hook.intercept({
-    register(tap) {
-        console.log(tap);
-        return tap
-    }
-})
+// hook.intercept({
+//     register(tap) {
+//         console.log(tap);
+//         return tap
+//     }
+// })
 
 hook.callAsync(122, (r) => {
+    console.log(r);
     return r
 })
--- src/webpack4/webpackTest.js
+++ src/webpack4/webpackTest.js
@@ -4,21 +4,6 @@ let config = require('./webpack.config')
 let compiler = webpack(config)
 
 compiler.hooks.compilation.tap('TT', (compilation, {normalModuleFactory}) => {
-    // console.log(normalModuleFactory);
-    compilation.hooks.buildModule.tap('TT', (module) => {
-        console.log('buildModule');
-    })
-
-    compilation.hooks.succeedModule.tap('TT', (module) => {
-        console.log('succeedModule');
-    })
-
-    compilation.hooks.failedModule.tap('TT', (module) => {
-        console.log('failedModule');
-    })
-    compilation.hooks.finishModules.tap('TT', (modules) => {
-        console.log('finishModules');
-    })
 
     const handler = (parser) => {
         parser.hooks.program.tap('TT', ast => {
@@ -29,6 +14,10 @@ compiler.hooks.compilation.tap('TT', (compilation, {normalModuleFactory}) => {
     normalModuleFactory.hooks.parser.for("javascript/auto").tap("TT", handler);
     normalModuleFactory.hooks.parser.for("javascript/dynamic").tap("TT", handler);
     normalModuleFactory.hooks.parser.for("javascript/esm").tap("TT", handler);
+})
+
+compiler.hooks.done.tap('TT', (stats)=> {
+    console.log(stats);
 })
 
 compiler.run((err) => {
