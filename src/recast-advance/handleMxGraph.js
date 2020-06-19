cf1c109c25e73b4ad1cb06682f0117e82060e5ee
jiangyukun <187_6810_5877@sina.com>
2020-06-19 15:40:33 +0800

    添加注释、统计信息

--- src/recast-advance/handleMxGraph.js
+++ src/recast-advance/handleMxGraph.js
@@ -10,130 +10,144 @@ let config = require('../recast/config')
 let {writeCodeToFile} = utils
 
 function getImportStr(filePath, moduleName, code) {
-    return handleModuleDep(code, moduleName).reduce((str, current) => {
-        let modulePath = handleModulePath(filePath, current)
-        if (modulePath) {
-            return str + `import ${current} from '${modulePath}'\n`
-        }
-        return str
-    }, '')
+  return handleModuleDep(code, moduleName).reduce((str, current) => {
+    let modulePath = handleModulePath(filePath, current)
+    if (modulePath) {
+      return str + `import ${current} from '${modulePath}'\n`
+    }
+    return str
+  }, '')
 }
 
 function handleModulePath(currentModulePath, depModule) {
-    let relativePath = currentModulePath.substring(currentModulePath.indexOf('/js/') + 4)
-    let count = relativePath.split('/').length - 1
-    let path = ''
-    for (let i = 0; i < count; i++) {
-        path += '../'
+  let relativePath = currentModulePath.substring(currentModulePath.indexOf('/js/') + 4)
+  let count = relativePath.split('/').length - 1
+  let path = ''
+  for (let i = 0; i < count; i++) {
+    path += '../'
+  }
+  let depPath = findModule(config.mxGraphRoot, depModule)
+  if (!depPath) {
+    depPath = _findModuleInner(config.mxGraphRoot, depModule)
+  }
+  if (depPath) {
+    let t = depPath.substring(currentModulePath.indexOf('/js/') + 4)
+    if (t == 'odel/mxGraphModel') {
+      console.log(1)
     }
-    let depPath = findModule(config.mxGraphRoot, depModule)
-    if (!depPath) {
-        depPath = _findModuleInner(config.mxGraphRoot, depModule)
-    }
-    if (depPath) {
-        let t = depPath.substring(currentModulePath.indexOf('/js/') + 4)
-        if (t == 'odel/mxGraphModel') {
-            console.log(1);
-        }
-        t = t.substring(0, t.length - 3)
-        path += t
-    }
-    if (depPath) {
-        return path
-    }
-    return null
+    t = t.substring(0, t.length - 3)
+    path += t
+  }
+  if (depPath) {
+    return path
+  }
+  return null
 }
 
 function findModule(fileDir, moduleName) {
-    let list = fs.readdirSync(fileDir)
-    for (let fileName of list) {
-        let filePath = fileDir + '/' + fileName
-        let stat = fs.statSync(filePath)
-        if (stat && stat.isDirectory()) {
-            // 递归子文件夹
-            let path = findModule(filePath, moduleName)
-            if (path) {
-                return path
-            }
-        } else {
-            if (filePath.endsWith(moduleName + '.js')) {
-                return filePath
-            }
-        }
+  let list = fs.readdirSync(fileDir)
+  for (let fileName of list) {
+    let filePath = fileDir + '/' + fileName
+    let stat = fs.statSync(filePath)
+    if (stat && stat.isDirectory()) {
+      // 递归子文件夹
+      let path = findModule(filePath, moduleName)
+      if (path) {
+        return path
+      }
+    } else {
+      if (filePath.endsWith(moduleName + '.js')) {
+        return filePath
+      }
     }
-    return null
+  }
+  return null
 }
 
 function handleModuleDep(code, moduleName) {
-    let moduleList = []
-    let ast = recast.parse(code)
-    recast.visit(ast, {
-        visitIdentifier(path) {
-            let identify = path.value
-            let name = identify.name
-            if (name.startsWith('mx') && name != 'mxLoadResources' && name != 'mxTransient') {
-                if (moduleList.indexOf(name) == -1 && name != moduleName) {
-                    moduleList.push(name)
-                }
-            }
-            return false
-        },
-        visitFunctionDeclaration(path) {
-            if (path.value.id.name != moduleName) {
-                return false
-            } else {
-                this.traverse(path)
-            }
+  let moduleList = []
+  let ast = recast.parse(code)
+  recast.visit(ast, {
+    visitIdentifier(path) {
+      let identify = path.value
+      let name = identify.name
+      if (name.startsWith('mx') && name != 'mxLoadResources' && name != 'mxTransient') {
+        if (moduleList.indexOf(name) == -1 && name != moduleName) {
+          moduleList.push(name)
         }
-    })
-    return moduleList
+      }
+      return false
+    },
+    visitFunctionDeclaration(path) {
+      if (path.value.id.name != moduleName) {
+        return false
+      } else {
+        this.traverse(path)
+      }
+    }
+  })
+  return moduleList
 }
 
 let handledModuleList = []
+let srcTotalLine = 0
 
 function reserveFile(dir) {
-    let list = fs.readdirSync(dir)
-    list.forEach(function (fileName) {
-        let filePath = dir + '/' + fileName
-        let stat = fs.statSync(filePath)
-        if (stat && stat.isDirectory()) {
-            // 递归子文件夹
-            reserveFile(filePath)
-        } else {
-            if (!filePath.endsWith('.js')) {
-                return
-            }
-            let moduleName = fileName.substring(0, fileName.length - 3)
-            let code = fs.readFileSync(filePath).toString()
-            let resultInfo = handleModule(moduleName, code, filePath)
-            handledModuleList.push(filePath)
+  let list = fs.readdirSync(dir)
+  list.forEach(function (fileName) {
+    let filePath = dir + '/' + fileName
+    let stat = fs.statSync(filePath)
+    if (stat && stat.isDirectory()) {
+      // 递归子文件夹
+      reserveFile(filePath)
+    } else {
+      if (!filePath.endsWith('.js')) {
+        return
+      }
+      let moduleName = fileName.substring(0, fileName.length - 3)
+      let code = fs.readFileSync(filePath).toString()
+      let resultInfo = handleModule(moduleName, code, filePath)
+      handledModuleList.push(filePath)
 
-            if (resultInfo.otherModules.length > 0) {
-                resultInfo.otherModules.forEach(moduleName => {
-                    let otherModulePath = (dir + '/' + moduleName + '.js')
-                    if (handledModuleList.indexOf(otherModulePath) != -1) {
-                        console.log(`${moduleName} 重复的模块`)
-                    } else {
-                        handledModuleList.push(otherModulePath)
-                        handleModule(moduleName, code, otherModulePath)
-                    }
+      let beforeLine = code.split('\n').length
+      srcTotalLine += beforeLine
+      let afterLine = resultInfo.code.split('\n').length
 
-                })
-            }
-        }
-    })
+      if (resultInfo.otherModules.length > 0) {
+        resultInfo.otherModules.forEach(subModuleName => {
+          let otherModulePath = (dir + '/' + subModuleName + '.js')
+          if (handledModuleList.indexOf(otherModulePath) != -1) {
+            // console.log(`${filePath} ${subModuleName} 重复的模块`)
+          } else {
+            handledModuleList.push(otherModulePath)
+            let subModuleResult = handleModule(subModuleName, code, otherModulePath)
+            afterLine += subModuleResult.code.split('\n').length
+          }
+        })
+      }
+      let percent = ((beforeLine - afterLine) / beforeLine * 100).toFixed(2)
+      let bLine = beforeLine.toString().padStart(5)
+      let aLine = afterLine.toString().padStart(5)
+      let percentStr = percent.toString().padStart(5)
+      let showWarning = percent > 5
+      console.log(`${moduleName.padEnd(32, ' ')} -- ${bLine} => ${aLine} (${bLine - afterLine} ${percentStr}%) ${showWarning ? '!!!' : ''}`)
+    }
+  })
 }
 
 function handleModule(moduleName, code, filePath) {
-    let ast = recast.parse(code)
+  let ast = recast.parse(code)
 
-    let resultInfo = handlePrototype(moduleName, ast, {
-        findImport: () => getImportStr(filePath, moduleName, code)
-    })
-    let convertCode = resultInfo.code
-    let distPath = filePath.replace('src', 'dist')
-    writeCodeToFile(distPath, convertCode)
-    return resultInfo
+  let resultInfo = handlePrototype(moduleName, ast, {
+    findImport: () => getImportStr(filePath, moduleName, code)
+  })
+  let convertCode = resultInfo.code
+
+  let distPath = filePath.replace('src', 'dist')
+  writeCodeToFile(distPath, convertCode)
+  return resultInfo
 }
 
 reserveFile(config.mxGraphRoot)
+
+console.log('源码共：' + srcTotalLine + ' 行')
--- src/recast/handlePrototype.js
+++ src/recast/handlePrototype.js
@@ -6,182 +6,214 @@ let {classDeclaration, classProperty, identifier, classBody, methodDefinition, a
 let {spreadElement, arrayExpression} = builders
 
 function handlePrototype(moduleName, ast, options) {
-    let bodyAst = []
+  let bodyAst = []
 
-    let findImport = options.findImport || (() => '')
-    let newClassName = options.newClassName || moduleName
+  let findImport = options.findImport || (() => '')
+  let newClassName = options.newClassName || moduleName
 
-    let unHandlePropertyList = []
-    let otherModules = recastUtil.getOtherModuleList(moduleName, ast)
-    let superClass = options.superClass || recastUtil.getSuperClass(moduleName, ast)
-    let constructor = recastUtil.getConstructor(moduleName, ast, superClass)
-    let func = recastUtil.getFunction(moduleName, ast)
+  let unHandlePropertyList = []
+  let otherModules = recastUtil.getOtherModuleList(moduleName, ast)
+  let superClass = options.superClass || recastUtil.getSuperClass(moduleName, ast)
+  let constructor = recastUtil.getConstructor(moduleName, ast, superClass)
+  let func = recastUtil.getFunction(moduleName, ast)
 
-    recast.visit(ast, {
-        // visitIdentifier(path) {
-        //     let value = path.value
-        //     if (value.name == 'arguments') {
-        //         let parentPath = recastUtil.findParentPath(path, 'FunctionExpression')
-        //         path.replace(arrayExpression(parentPath.value.params))
-        //     }
-        //     return false
-        // },
-        visitObjectExpression() {
-            return false
-        },
-        visitCallExpression(path) {
-            let object = path.value.callee.object
-            let property = path.value.callee.property
-            let argumentList = path.value.arguments
-            if (object) {
-                if (object.name == 'mxUtils' && property.name == 'bind') {
-                    if (argumentList[1].type == 'FunctionExpression' && recastUtil.canUseArrowExpression(this, path)) {
-                        path.replace(arrowFunctionExpression(argumentList[1].params, argumentList[1].body, false))
-                    }
-                }
-            }
-            if (!superClass) {
-                return this.traverse(path)
-            }
+  let classComments = []
 
-            if (object && property && property.name == 'apply') {
-                if (object.type == 'Identifier') {
-                    if (argumentList.length == 2 && argumentList[0].type == 'ThisExpression') {
-                        let parentPath = recastUtil.findParentPath(path, 'AssignmentExpression')
-                        if (parentPath && parentPath.value.left.property) {
-                            let leftCode = recast.print(parentPath.value.left).code
-                            let sameFunction = leftCode.toLowerCase().indexOf(object.name.toLowerCase()) != -1
-                            if (sameFunction && leftCode.indexOf(`${moduleName}.prototype.`) != -1) {
-                                console.log(recast.print(path.value).code)
-                                let methodName = parentPath.value.left.property.name
-                                if (argumentList[1]) {
-                                    path.replace(callExpression(memberExpression(identifier('super'), identifier(methodName)), [spreadElement(argumentList[1])]))
-                                } else {
-                                    path.replace(callExpression(memberExpression(identifier('super'), identifier(methodName)), []))
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    let leftCode = recast.print(object).code
-                    if (leftCode.indexOf(`${superClass.name}.prototype.`) != -1) {
-                        console.log(leftCode);
-                        let name = leftCode.split('.')[2]
-                        if (argumentList[1]) {
-                            path.replace(callExpression(memberExpression(identifier('super'), identifier(name)), [spreadElement(identifier('arguments'))]))
-                        } else {
-                            path.replace(callExpression(memberExpression(identifier('super'), identifier(name)), []))
-                        }
-                    }
-                }
-            }
-            this.traverse(path)
-        },
-        visitAssignmentExpression(path) {
-            if (recastUtil.isInExpression(path, 'FunctionExpression')) {
-                return this.traverse(path)
-            }
-            let leftCode = recast.print(path.value.left).code
-            let right = path.value.right
-
-            if (leftCode.startsWith(`${moduleName}.prototype.`)) {
-                let property = leftCode.substring(`${moduleName}.prototype`.length + 1)
-                if (!property || property == 'constructor') {
-                    return false
-                }
-                if (right.type == 'FunctionExpression') {
-                    bodyAst.push(methodDefinition('method', identifier(property), right))
-                } else {
-                    bodyAst.push(classProperty(identifier(property), right))
-                }
-            } else if (leftCode.startsWith(`${moduleName}.`)) {
-                let property = leftCode.substring(`${moduleName}`.length + 1)
-                if (property === 'prototype') {
-                    return false
-                }
-                if (right.type == 'FunctionExpression') {
-                    bodyAst.push(methodDefinition('method', identifier(property), right, true))
-                } else {
-                    bodyAst.push(classProperty(identifier(property), right, null, true))
-                }
-            } else if (leftCode.indexOf('.prototype.') != -1) {
-                unHandlePropertyList.push(leftCode)
-            }
-            this.traverse(path)
+  recast.visit(ast, {
+    // visitIdentifier(path) {
+    //     let value = path.value
+    //     if (value.name == 'arguments') {
+    //         let parentPath = recastUtil.findParentPath(path, 'FunctionExpression')
+    //         path.replace(arrayExpression(parentPath.value.params))
+    //     }
+    //     return false
+    // },
+    visitObjectExpression() {
+      return false
+    },
+    visitFunctionDeclaration(path) {
+      let value = path.value
+      if (value.id.name == newClassName) {
+        classComments = value.comments
+      }
+      return false
+    },
+    visitCallExpression(path) {
+      let object = path.value.callee.object
+      let property = path.value.callee.property
+      let argumentList = path.value.arguments
+      if (object) {
+        if (object.name == 'mxUtils' && property.name == 'bind') {
+          let parentValue = path.parentPath.value
+          let toName = '', line = 0
+          if (parentValue.type == 'VariableDeclarator') {
+            toName = parentValue.id.name
+          }
+          if (parentValue instanceof Array) {
+            line = parentValue[0].loc.start.line
+          } else {
+            line = parentValue.loc.start.line
+          }
+          if (argumentList[1].type == 'FunctionExpression' && recastUtil.canUseArrowExpression(this, path)) {
+            // console.log(`处理bind函数 -- ${newClassName} ${toName}(${line})`)
+            path.replace(arrowFunctionExpression(argumentList[1].params, argumentList[1].body, false))
+          }
         }
-    })
+      }
+      if (!superClass) {
+        return this.traverse(path)
+      }
 
-    if (func && bodyAst.length == 0) {
-        // 普通函数
-        return {code: findImport() + '\n' + recastUtil.getCode(func), otherModules}
-    } else if (bodyAst.length > 0) {
-        if (constructor) {
-            bodyAst.unshift(constructor)
+      if (object && property && property.name == 'apply') {
+        if (object.type == 'Identifier') {
+          if (argumentList.length == 2 && argumentList[0].type == 'ThisExpression') {
+            let parentPath = recastUtil.findParentPath(path, 'AssignmentExpression')
+            if (parentPath && parentPath.value.left.property) {
+              let leftCode = recast.print(parentPath.value.left).code
+              let sameFunction = leftCode.toLowerCase().indexOf(object.name.toLowerCase()) != -1
+              if (sameFunction && leftCode.indexOf(`${moduleName}.prototype.`) != -1) {
+                // console.log(22, recast.print(path.value).code)
+                let methodName = parentPath.value.left.property.name
+                if (argumentList[1]) {
+                  path.replace(callExpression(memberExpression(identifier('super'), identifier(methodName)), [spreadElement(argumentList[1])]))
+                } else {
+                  path.replace(callExpression(memberExpression(identifier('super'), identifier(methodName)), []))
+                }
+              }
+            }
+          }
+        } else {
+          let leftCode = recast.print(object).code
+          if (leftCode.indexOf(`${superClass.name}.prototype.`) != -1) {
+            let line = path.parentPath.value.loc.start.line
+            // console.log(`处理super -- ${newClassName} -- ${leftCode} (${line})`)
+            let name = leftCode.split('.')[2]
+            if (argumentList[1]) {
+              path.replace(callExpression(memberExpression(identifier('super'), identifier(name)), [spreadElement(identifier('arguments'))]))
+            } else {
+              path.replace(callExpression(memberExpression(identifier('super'), identifier(name)), []))
+            }
+          }
         }
-        if (unHandlePropertyList.length != 0) {
-            // console.log(moduleName, unHandlePropertyList)
+      }
+      this.traverse(path)
+    },
+    visitAssignmentExpression(path) {
+      if (recastUtil.isInExpression(path, 'FunctionExpression')) {
+        return this.traverse(path)
+      }
+      let leftCode = recast.print(path.value.left).code
+      let right = path.value.right
+
+      if (leftCode.startsWith(`${moduleName}.prototype.`)) {
+        let property = leftCode.substring(`${moduleName}.prototype`.length + 1)
+        if (!property || property == 'constructor') {
+          return false
         }
-        let convertCode = recastUtil.getCode(classDeclaration(identifier(newClassName), classBody(bodyAst), superClass)) + `
+        if (right.type == 'FunctionExpression') {
+          let comments = path.parentPath.value.comments
+          let methodDef = methodDefinition('method', identifier(property), right)
+          methodDef.comments = comments
+          bodyAst.push(methodDef)
+        } else {
+          let comments = path.parentPath.value.comments
+          let classDef = classProperty(identifier(property), right)
+          classDef.comments = comments
+          bodyAst.push(classDef)
+        }
+      } else if (leftCode.startsWith(`${moduleName}.`)) {
+        let property = leftCode.substring(`${moduleName}`.length + 1)
+        if (property === 'prototype') {
+          return false
+        }
+        if (right.type == 'FunctionExpression') {
+          let comments = path.parentPath.value.comments
+          let methodDef = methodDefinition('method', identifier(property), right, true)
+          methodDef.comments = comments
+          bodyAst.push(methodDef)
+        } else {
+          bodyAst.push(classProperty(identifier(property), right, null, true))
+        }
+      } else if (leftCode.indexOf('.prototype.') != -1) {
+        unHandlePropertyList.push(leftCode)
+      }
+      this.traverse(path)
+    }
+  })
+
+  if (func && bodyAst.length == 0) {
+    // 普通函数
+    return {code: findImport() + '\n' + recastUtil.getCode(func), otherModules}
+  } else if (bodyAst.length > 0) {
+    if (constructor) {
+      bodyAst.unshift(constructor)
+    }
+    if (unHandlePropertyList.length != 0) {
+      // console.log(moduleName, unHandlePropertyList)
+    }
+    let classDec = classDeclaration(identifier(newClassName), classBody(bodyAst), superClass)
+    classDec.comments = classComments
+    let convertCode = recastUtil.getCode(classDec) + `
 
 export default ${newClassName}
 `
-        return {code: findImport() + '\n' + convertCode, otherModules}
-    } else {
-        // 常量类
-        let returnOldCode = false
-        recast.visit(ast, {
-            visitVariableDeclaration(path) {
-                let value = path.value
-                let declaration = value.declarations[0]
-                if (declaration.id.name == moduleName) {
-                    returnOldCode = true
-                }
-                return false
-            },
-            visitFunctionDeclaration() {
-                return false
-            },
-            visitCallExpression() {
-                return false
-            }
-        })
-        if (returnOldCode) {
-            return {
-                code: findImport() + '\n' + recast.print(ast).code + `\n export default ${newClassName}`,
-                otherModules
-            }
-        } else {
-            let isRegister = false
-            recast.visit(ast, {
-                visitFunctionExpression() {
-                    return false
-                },
-                visitFunctionDeclaration() {
-                    return false
-                },
-                visitCallExpression(path) {
-                    let value = path.value
-                    let callee = value.callee
-
-                    try {
-                        if (callee.object.name == 'mxCodecRegistry' && callee.property.name == 'register') {
-                            isRegister = true
-                        }
-                    } catch (e) {
-                        console.log(1);
-                    }
-                    return false
-                }
-            })
-            if (isRegister) {
-                return {code: findImport() + '\n' + recast.print(ast).code, otherModules}
-            } else {
-                console.log(moduleName + '不是一个模块')
-                return {otherModules, code: null}
-            }
-
+    return {code: findImport() + '\n' + convertCode, otherModules}
+  } else {
+    // 常量类
+    let returnOldCode = false
+    recast.visit(ast, {
+      visitVariableDeclaration(path) {
+        let value = path.value
+        let declaration = value.declarations[0]
+        if (declaration.id.name == moduleName) {
+          returnOldCode = true
         }
+        return false
+      },
+      visitFunctionDeclaration() {
+        return false
+      },
+      visitCallExpression() {
+        return false
+      }
+    })
+    if (returnOldCode) {
+      return {
+        code: findImport() + '\n' + recast.print(ast).code + `\n export default ${newClassName}`,
+        otherModules
+      }
+    } else {
+      let isRegister = false
+      recast.visit(ast, {
+        visitFunctionExpression() {
+          return false
+        },
+        visitFunctionDeclaration() {
+          return false
+        },
+        visitCallExpression(path) {
+          let value = path.value
+          let callee = value.callee
+
+          try {
+            if (callee.object.name == 'mxCodecRegistry' && callee.property.name == 'register') {
+              isRegister = true
+            }
+          } catch (e) {
+            console.log(1)
+          }
+          return false
+        }
+      })
+      if (isRegister) {
+        return {code: findImport() + '\n' + recast.print(ast).code, otherModules}
+      } else {
+        console.log(moduleName + '不是一个模块')
+        return {otherModules, code: null}
+      }
+
     }
+  }
 }
 
 module.exports = handlePrototype
