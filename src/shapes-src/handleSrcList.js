f76f032726dcf551216c94938495006af513834b
jiangyukun <187_6810_5877@sina.com>
2020-07-02 16:16:43 +0800

    处理元件

---
+++ src/shapes-src/handleSrcList.js
@@ -0,0 +1,35 @@
+let utils = require('../../utils/fileUtil')
+
+let list = '[\n'
+utils.reserveFile('D:/2066/Projects/ies-cloud-simulator/view-react/src/constants/svg-shapes', (path) => {
+  if (path.indexOf('.txt') != -1) {
+    return
+  }
+  let txtPath = path.replace('.svg', '.txt')
+  let name = ''
+  try {
+    let content = utils.getFileContent(txtPath)
+    let firstRow = content.split('\n')[0]
+    name = firstRow.split('：')[1]
+    if (!name) {
+      name = firstRow.split(';')[1]
+      if (!name) {
+        name = firstRow.split(':')[1]
+        if (!name) {
+          console.log(firstRow)
+        }
+      }
+    }
+  } catch (e) {
+    // console.log('找不到' + txtPath)
+  }
+  let p = path.replace(/\\/g, '/')
+  name = name.replace(/\n|\r/, '')
+  let item = `[require('./${p.substring(62)}'), './${p.substring(62)}', '${name}'],`
+  console.log(item)
+  list += item
+
+})
+
+list += '\n]'
+// console.log(list)
