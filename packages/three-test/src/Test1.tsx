17155d6eaead31f76560a3df0375c089b3cea6a1
jiangyukun <187_6810_5877@sina.com>
2020-07-22 13:57:42 +0800

    threejs 官方demo

--- packages/three-test/src/Test1.tsx
+++ packages/three-test/src/Test1.tsx
@@ -1,15 +1,45 @@
 import React, {useEffect, useRef} from 'react'
 
-function Test1(props) {
-  let ref = useRef()
+import * as three from 'three'
+
+const {PerspectiveCamera, Scene, BoxGeometry, MeshNormalMaterial, Mesh, WebGLRenderer} = three
+
+let render
+let camera
+let mesh
+let scene
+
+function animate() {
+  requestAnimationFrame(animate)
+  mesh.rotation.x += 0.01
+  mesh.rotation.y += 0.02
+  render.render(scene, camera)
+}
+
+function Test1() {
+  let ref = useRef<HTMLDivElement>()
 
   useEffect(() => {
 
+    camera = new PerspectiveCamera(70, 1, 0.01, 10)
+    camera.position.z = 1
+
+    scene = new Scene()
+
+    let geometry = new BoxGeometry(0.2, 0.2, 0.2)
+    let material = new MeshNormalMaterial()
+
+    mesh = new Mesh(geometry, material)
+    scene.add(mesh)
+
+    render = new WebGLRenderer({antialias: true})
+    render.setSize(300, 300)
+    ref.current.appendChild(render.domElement)
+    animate()
   }, [])
 
   return (
-    <div ref={ref}>
-
+    <div ref={ref} style={{width: 300, height: 300}}>
     </div>
   )
 }
